e578d2ba2d03b7047464ed8a67d30638265e0b20
chanli <10915819@qq.com>
2016-08-19 11:40:13 +0800

    fix 恢复文件

---
+++ Source/GoKit.h
@@ -0,0 +1,17 @@
+//
+//  GoKit.h
+//  GoKitDemo
+//
+//  Created by chanli on 16/8/19.
+//  Copyright © 2016年 Beijing ChunFengShiLi Technology Co., Ltd. All rights
+//  reserved.
+//
+
+#ifndef GoKit_h
+#define GoKit_h
+
+#import "UIButton+GoBlock.h"
+#import "UIImage+GoWaterMark.h"
+#import "UIView+GoBlur.h"
+
+#endif /* GoKit_h */
